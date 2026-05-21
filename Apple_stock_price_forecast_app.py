a9055c29d57d00b9170328b537f96dc1d8b57b18
Tilna <tilnakuriakose@gmail.com>
2026-05-21 21:22:10 +0530

    Update Apple_stock_price_forecast_app.py

--- Apple_stock_price_forecast_app.py
+++ Apple_stock_price_forecast_app.py
@@ -8,7 +8,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from tensorflow.keras.models import load_model
+from keras.models import load_model
 from sklearn.preprocessing import MinMaxScaler
 import warnings
 warnings.filterwarnings('ignore')
